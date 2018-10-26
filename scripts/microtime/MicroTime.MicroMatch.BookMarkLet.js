9987acad8d2e9fa91a2cb996e280f709c30591bd
jDevTeam2018 <40192129+jdevteam2018@users.noreply.github.com>
2018-10-26 15:11:23 -0500

    Create MicroTime.MicroMatch.BookMarkLet.js

---
+++ scripts/microtime/MicroTime.MicroMatch.BookMarkLet.js
@@ -0,0 +1,28 @@
+function onclick() {
+	setTimeout(function runBot() {
+		try {
+			var data = (((Quizlet || {}).matchModeData || {}).terms || []);
+			data.forEach((term)=>{
+				document.querySelectorAll('div').forEach(function(div) {
+					if(div.innerHTML == term.word) {
+						document.querySelectorAll('div').forEach(function(div2) {
+							if(div2.innerHTML == term.definition) {
+								var evObj = document.createEvent('Events');
+								evObj.initEvent('pointerdown', true, false);
+								div2.dispatchEvent(evObj);
+								div.dispatchEvent(evObj);
+							};
+						});
+					};
+				});
+			});
+		} catch(err) {
+			alert(err);
+		};
+	}, 500);
+};
+if(Quizlet.matchModeData.isReplaying) {
+	window.onload = onclick;
+} else {
+	window.onclick = onclick;
+};
