28f385ae26dc3c4a780d0e2b3509548ec7aaf3de
jDevTeam2018 <40192129+jdevteam2018@users.noreply.github.com>
2018-10-26 15:10:36 -0500

    Create MicroTime.MicroMatch.UserScript.js

---
+++ scripts/microtime/MicroTime.MicroMatch.UserScript.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         MicroTimez for MicroMatch
+// @namespace    MicroTimez
+// @version      1.0
+// @description  This bot get's 0.5 seconds most of the time, clicking "Play again" prevents the "Start game button", so in those cases it will get ~0.7-0.8 seconds.
+// @author       John Jelatis
+// @match        https://quizlet.com/*/micromatch
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+	function onclick() {
+		setTimeout(function runBot() {
+			try {
+				var data = (((Quizlet || {}).matchModeData || {}).terms || []);
+				data.forEach((term)=>{
+					document.querySelectorAll('div').forEach(function(div) {
+						if(div.innerHTML == term.word) {
+							document.querySelectorAll('div').forEach(function(div2) {
+								if(div2.innerHTML == term.definition) {
+									var evObj = document.createEvent('Events');
+									evObj.initEvent('pointerdown', true, false);
+									div2.dispatchEvent(evObj);
+									div.dispatchEvent(evObj);
+								};
+							});
+						};
+					});
+				});
+			} catch(err) {
+				alert(err);
+			};
+		}, 500);
+	};
+	if(Quizlet.matchModeData.isReplaying) {
+		window.onload = onclick;
+	} else {
+		window.onclick = onclick;
+	};
+})();
